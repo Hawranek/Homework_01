9f5b0e09ae8203627602d5b721fc2879124c1d79
Damian Karczmarz <damian.karczmarz92@gmail.com>
2021-06-12 17:07:38 +0200

    Zadanie 1

---
+++ src/main/java/pl/coderslab/beans/Customer.java
@@ -0,0 +1,12 @@
+package pl.coderslab.beans;
+
+public class Customer {
+//    Klasa ma mieć prywatne atrybuty:
+//
+//    id - atrybut ten powinien trzymać numer identyfikacyjny,
+//    firstName - atrybut określający imię klienta,
+//    lastName - atrybut określający nazwisko klienta,
+    private int id;
+    private String firstName;
+    private String lastName;
+}
